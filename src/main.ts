eb15c6a7d8088fe56d82a0dbbcf6ef393886348e
Desplay Shido <desplayshido@gmail.com>
2023-07-29 14:06:34 +0700

    update for deploy

--- src/main.ts
+++ src/main.ts
@@ -1,11 +1,14 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 import { graphqlUploadExpress } from 'graphql-upload';
+import * as csurf from 'csurf';
 
 async function bootstrap() {
   const post = process.env.PORT || 3000;
   const app = await NestFactory.create(AppModule);
   app.use(graphqlUploadExpress({ maxFileSize: 1000000, maxFiles: 10 }));
+  app;
   await app.listen(post);
+  app.use(csurf());
 }
 bootstrap();
